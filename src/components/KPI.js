203e5b9099ea3487b0f234975893b7671c0e85bc
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-09 18:42:32 +0100

    Réalisation partielle du 4ème graphique

--- src/components/KPI.js
+++ src/components/KPI.js
@@ -1,6 +1,16 @@
+import {PieChart, Pie} from 'recharts'
+
+const score1 = [{score: 0.12}, {score: 0.88}]
+const score2 = [{score: 0.3}, {score: 0.7}]
+
 function KPI() {
     return (
-        <div id="kpi" className="anaItem">composant "KPI"</div>
+        <div id="kpi" className="anaItem">
+            <PieChart width={250} height={250}>
+                <Pie data={score1} dataKey="score" cx="50%" cy="50%" innerRadius={70} outerRadius={80} fill="red"/>
+                <Pie data={score2} dataKey="score" cx="50%" cy="50%" innerRadius={50} outerRadius={60} fill="blue"/>
+            </PieChart>
+        </div>
     )
 }
 
