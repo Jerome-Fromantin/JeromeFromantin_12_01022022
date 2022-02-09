9d6adc0884b92eefd65951095f7fee6292385fa3
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-09 18:52:31 +0100

    Finition du 1er graphique

--- src/components/Poids.js
+++ src/components/Poids.js
@@ -1,102 +1,56 @@
+import { useEffect, useState } from 'react'
+import { getPoidsData } from '../services/services.mock'
+//import { getPoidsData } from '../services/services'
 import {BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts'
 import barchartLegend from '../images/barchartLegend.png'
 
-// POINTS A COMPLETER :
-// - Modifier le contenu du tooltip.
-// - Utiliser les vraies données sur "data.js".
-
-const sessions1 = [
-    {day: '2020-07-01', kilogram: 80, calories: 240},
-    {day: '2020-07-02', kilogram: 80, calories: 220},
-    {day: '2020-07-03', kilogram: 81, calories: 280},
-    {day: '2020-07-04', kilogram: 81, calories: 290},
-    {day: '2020-07-05', kilogram: 80, calories: 160},
-    {day: '2020-07-06', kilogram: 78, calories: 162},
-    {day: '2020-07-07', kilogram: 76, calories: 390}
-]
-
-const sessions2 = [
-    {
-        day: '2020-07-01',
-        kilogram: 70,
-        calories: 240
-    },
-    {
-        day: '2020-07-02',
-        kilogram: 69,
-        calories: 220
-    },
-    {
-        day: '2020-07-03',
-        kilogram: 70,
-        calories: 280
-    },
-    {
-        day: '2020-07-04',
-        kilogram: 70,
-        calories: 500
-    },
-    {
-        day: '2020-07-05',
-        kilogram: 69,
-        calories: 160
-    },
-    {
-        day: '2020-07-06',
-        kilogram: 69,
-        calories: 162
-    },
-    {
-        day: '2020-07-07',
-        kilogram: 69,
-        calories: 390
-    }
-]
-
+/* This function gets the number of the day, to be used in the X axis of the chart. */
 const getDayNumber = (date) => {
     const dayNumber = new Date(date);
     return dayNumber.getDate()
 }
 
-const tooltip = {
-    background: "#E60000",
-    color: "white"
-}
-
-const tooltipValues = {
-    color: "white"
-}
-
-const customTooltip = ({active, payload, dataKey, yAxisId}) => {
+const customTooltip = ({active, payload}) => {
     if (active && payload && payload.length) {
-        //if (dataKey === "kilogram") {
-            return (
-                <div>
-                    <p>{`${payload[0].value} kg`}</p>
-                </div>
-            )
-        //}
-        /*if (yAxisId === "right") {
-            return (
-                <div>
-                    <p>{`${payload[0].value} kCal`}</p>
-                </div>
-            )
-        }*/
-        //return "Pas bon !"
+        return (
+            <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            background: "#E60000",
+            height: 63,
+            color: "white",
+            fontSize: 10,
+            fontWeight: 500,
+            textAlign: "center",
+            padding: 5}}>
+                <p>{`${payload[0].value} kg`}</p>
+                <p>{`${payload[1].value} kCal`}</p>
+            </div>
+        )
     }
-    return "Ca ne marche pas !" //null
+    return null
 }
 
-function Poids() {
+function Poids(props) {
+    const [poidsData, setPoidsData] = useState([])
+
+    useEffect(() => {
+        async function init() {
+            const sessions = await getPoidsData(props.id)
+            setPoidsData(sessions)
+        }
+        init()
+    }, [props.id])
+
     return (
         <div id="poids" className="anaItem">
-            <BarChart width={805} height={320} data={sessions1} barGap={8} barSize={7}
+            <BarChart width={805} height={320} data={poidsData} barGap={8} barSize={7}
             margin={{top: 90, right: 0, bottom: 25, left: 33}} className="graphCenter">
                 <CartesianGrid strokeDasharray="2" vertical={false}/>
                 <XAxis dataKey="day" axisLine={false} padding={{left: -49, right: -49}}
                 tickFormatter={getDayNumber} tickLine={false} tickMargin={15}/>
-                <Tooltip /*content={customTooltip}*/ contentStyle={tooltip} itemStyle={tooltipValues} offset={30}/>
+                <Tooltip content={customTooltip} offset={30}/>
                 <YAxis yAxisId="left" axisLine={false} domain={["dataMin - 1", "dataMax"]} orientation="right" tickCount={3} tickLine={false} tickMargin={25}/>
                 <YAxis yAxisId="right" axisLine={false} domain={["dataMin - 10", "dataMax"]} mirror={true} tickCount={0} tickLine={false}/>
                 <Bar yAxisId="left" dataKey="kilogram" fill="#282D30"/>
