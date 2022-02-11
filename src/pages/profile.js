606095ba2dfd977764fb76370a71388b61c80e2a
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-11 17:38:04 +0100

    Amélioration de l'accès aux données mockées

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,7 @@
 import "../styles.css"
+import { useEffect, useState } from 'react'
+import { getKeyData } from '../services/services.mock'
+//import { getKeyData } from '../services/services'
 import {useParams} from "react-router-dom"
 import Header from "../components/Header"
 import Poids from "../components/Poids"
@@ -12,23 +15,37 @@ import glucIcon from "../images/gluc-icon.png"
 import lipiIcon from "../images/lipi-icon.png"
 
 function Profile(props) {
+    const [keyData, setKeyData] = useState([])
+
+    useEffect(() => {
+        async function init() {
+            const rightCompData = await getKeyData(props.id)
+            setKeyData(rightCompData)
+        }
+        init()
+    }, [props.id])
+
     const param = useParams()
 
     return (
         <div>
-            <Header/>
+            <Header id={param.id}/>
             <div className="analytics">
                 <div className="analyticsLeft">
                     <Poids id={param.id}/>
                     <div className="leftDown">
-                        <Objectifs/>
-                        <RadarData/>
-                        <KPI/>
+                        <Objectifs id={param.id}/>
+                        <RadarData id={param.id}/>
+                        <KPI id={param.id}/>
                     </div>
                 </div>
                 <div className="analyticsRight">
-                    <RightComponent compoId="calo" iconSrc={caloIcon} iconAlt="Icône calories"
-                    compoValue="1,930kCal" compoType="Calories"/>
+                    {keyData.calorieCount ?
+                        <RightComponent id={param.id} compoId="calo" iconSrc={caloIcon} iconAlt="Icône calories"
+                        compoValue={`${keyData.calorieCount}kCal`} compoType="Calories"/> :
+                        <RightComponent id={param.id} compoId="calo" iconSrc={caloIcon} iconAlt="Icône calories"
+                        compoValue="000kCal" compoType="Calories"/>
+                    }
                     <RightComponent compoId="prot" iconSrc={protIcon} iconAlt="Icône protéines"
                     compoValue="155g" compoType="Protéines"/>
                     <RightComponent compoId="gluc" iconSrc={glucIcon} iconAlt="Icône glucides"
