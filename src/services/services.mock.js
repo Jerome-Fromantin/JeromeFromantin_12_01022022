9fa41b67045c9295c73a11facf456ce059f4a020
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-11 17:56:44 +0100

    Ajout de fonctions d'accès aux données mockées

--- src/services/services.mock.js
+++ src/services/services.mock.js
@@ -1,6 +1,31 @@
-import {USER_ACTIVITY} from "./data"
+import {USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE} from "./data"
+
+export async function getFirstName(id) {
+    const firstName = USER_MAIN_DATA.find(item => item.id == id)
+    return firstName.userInfos
+}
 
 export async function getPoidsData(userId) {
     const poidsData = USER_ACTIVITY.find(item => item.userId == userId)
     return poidsData.sessions
+}
+
+export async function getObjectifsData(userId) {
+    const objectifsData = USER_AVERAGE_SESSIONS.find(item => item.userId == userId)
+    return objectifsData.sessions
+}
+
+export async function getPerformanceData(userId) {
+    const performanceData = USER_PERFORMANCE.find(item => item.userId == userId)
+    return performanceData.data
+}
+
+export async function getScore(id) {
+    const userScore = USER_MAIN_DATA.find(item => item.id == id)
+    return userScore.score
+}
+
+export async function getKeyData(id) {
+    const rightCompData = USER_MAIN_DATA.find(item => item.id == id)
+    return rightCompData.keyData
 }
