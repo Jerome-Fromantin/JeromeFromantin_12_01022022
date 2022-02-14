782393aa33eccea894c843dfb74c63ec80dc7337
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-14 17:41:59 +0100

    Ajout d'un tooltip à améliorer

--- src/components/Radar.js
+++ src/components/Radar.js
@@ -1,7 +1,30 @@
 import { useEffect, useState } from 'react'
 import { getPerformanceData } from '../services/services.mock'
 //import { getPerformanceData } from '../services/services'
-import {RadarChart, Radar, PolarGrid, PolarAngleAxis} from 'recharts'
+import {RadarChart, Radar, PolarGrid, PolarAngleAxis, Tooltip} from 'recharts'
+
+const kindFR = {1: "Cardio", 2: "Energie", 3: "Endurance", 4: "Force", 5: "Vitesse", 6: "Intensité"}
+const getTheme = (index) => {
+    return kindFR[index]
+}
+
+/* This function gives to the tooltip a customized content with its own style. */
+const customTooltip = ({active, payload}) => {
+    if (active && payload && payload.length) {
+        return (
+            <div style={{
+            background: "white",
+            color: "black",
+            fontSize: 10,
+            fontWeight: 500,
+            textAlign: "center",
+            padding: 5}}>
+                <p>{`${payload[0].kind} : ${payload[0].value}`}</p>
+            </div>
+        )
+    }
+    return null
+}
 
 function RadarData(props) {
     const [performanceData, setPerformanceData] = useState([])
@@ -14,11 +37,6 @@ function RadarData(props) {
         init()
     }, [props.id])
 
-    const kindFR = {1: "Cardio", 2: "Energie", 3: "Endurance", 4: "Force", 5: "Vitesse", 6: "Intensité"}
-    const getTheme = (index) => {
-        return kindFR[index]
-    }
-
     const performanceDataReverse = [...performanceData].sort((a, b) => b.kind - a.kind)
 
     return (
@@ -28,6 +46,8 @@ function RadarData(props) {
                 <PolarGrid radialLines={false}/>
                 <PolarAngleAxis dataKey="kind" fontSize={12} tickFormatter={getTheme} tickLine={false} stroke="white"/>
                 <Radar dataKey="value" fill="rgb(255, 1, 1)" fillOpacity={0.7}/>
+                <Tooltip content={customTooltip}
+                cursor={{stroke: "white", strokeOpacity : 0.2, strokeWidth: 10}} offset={30}/>
             </RadarChart>
         </div>
     )
