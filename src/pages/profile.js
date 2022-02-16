60a48fed092a9f02cb08d590a0a9d255b216b204
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-16 18:23:53 +0100

    Improvement of the page & adding of English documentation

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,39 +1,48 @@
 import "../styles.css"
 import { useEffect, useState } from 'react'
-import { getMainData } from '../services/services.mock'
-//import { getMainData } from '../services/services'
 import {useParams} from "react-router-dom"
+
+/* ACCESS TO DATA */
+// Access to mocked data in the file "src/services/data.js"
+//import { getMainData } from '../services/services.mock'
+
+// Access to "real" data in the backend API
+import { getMainData } from '../services/services'
+
+// Access to a part of data, mocked or not
+import Formatter from '../services/formatter'
+
+/* ACCESS TO COMPONENTS */
 import Header from "../components/Header"
 import Poids from "../components/Poids"
 import Objectifs from "../components/Objectifs"
 import RadarData from "../components/Radar"
 import KPI from "../components/KPI"
 import RightComponent from "../components/RightComponent"
+
+/* ACCESS TO ICONS FOR THE COMPONENTS ON THE RIGHT */
 import caloIcon from "../images/calo-icon.png"
 import protIcon from "../images/prot-icon.png"
 import glucIcon from "../images/gluc-icon.png"
 import lipiIcon from "../images/lipi-icon.png"
 
 function Profile(props) {
-    const [mainData, setMainData] = useState([])
+    const param = useParams()
+    const [keyData, setKeyData] = useState({})
 
+    // Use of "AbortController()" to prevent memory leaks
     useEffect(() => {
         let abortController = new AbortController()
         async function init() {
-            const mainDatas = await getMainData(props.id)
-            setMainData(mainDatas)
+            const mainData = await getMainData(param.id)
+            const formatter = new Formatter(mainData)
+            setKeyData(formatter.getKeyData())
         }
         init()
         return () => {
             abortController.abort()
         }
-    }, [props.id])
-
-    //console.log(mainData.keyData)
-    //const userInfo = mainData.userInfos
-    //console.log(userInfo)
-
-    const param = useParams()
+    }, [param.id])
 
     return (
         <div>
@@ -48,20 +57,14 @@ function Profile(props) {
                     </div>
                 </div>
                 <div className="analyticsRight">
-                    {/*{mainData.calorieCount ?
-                        <RightComponent id={param.id} compoId="calo" iconSrc={caloIcon} iconAlt="Icône calories"
-                        compoValue={`${mainData.calorieCount}kCal`} compoType="Calories"/> :
-                        <RightComponent id={param.id} compoId="calo" iconSrc={caloIcon} iconAlt="Icône calories"
-                        compoValue="000kCal" compoType="Calories"/>
-                    }*/}
                     <RightComponent id={param.id} compoId="calo" iconSrc={caloIcon} iconAlt="Icône calories"
-                    compoValue={mainData} compoType="Calories"/>
-                    <RightComponent compoId="prot" iconSrc={protIcon} iconAlt="Icône protéines"
-                    compoValue="155g" compoType="Protéines"/>
-                    <RightComponent compoId="gluc" iconSrc={glucIcon} iconAlt="Icône glucides"
-                    compoValue="290g" compoType="Glucides"/>
-                    <RightComponent compoId="lipi" iconSrc={lipiIcon} iconAlt="Icône lipides"
-                    compoValue="50g" compoType="Lipides"/>
+                    compoValue={`${keyData.calorieCount}kCal`} compoType="Calories"/>
+                    <RightComponent id={param.id} compoId="prot" iconSrc={protIcon} iconAlt="Icône protéines"
+                    compoValue={`${keyData.proteinCount}g`} compoType="Protéines"/>
+                    <RightComponent id={param.id} compoId="gluc" iconSrc={glucIcon} iconAlt="Icône glucides"
+                    compoValue={`${keyData.carbohydrateCount}g`} compoType="Glucides"/>
+                    <RightComponent id={param.id} compoId="lipi" iconSrc={lipiIcon} iconAlt="Icône lipides"
+                    compoValue={`${keyData.lipidCount}g`} compoType="Lipides"/>
                 </div>
             </div>
         </div>
