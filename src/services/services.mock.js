29e9412810751ec7124aa7bb19edd6349be5f0d1
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-16 18:48:36 +0100

    Improvement to allow access to database, mocked or not

--- src/services/services.mock.js
+++ src/services/services.mock.js
@@ -1,27 +1,21 @@
 import {USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE} from "./data"
 
 export async function getMainData(id) {
-    const mainData = USER_MAIN_DATA.find(item => item.id == id)
-    console.log(mainData)
+    const mainData = USER_MAIN_DATA.find(item => item.id === Number(id))
     return mainData
 }
 
 export async function getPoidsData(userId) {
-    const poidsData = USER_ACTIVITY.find(item => item.userId == userId)
+    const poidsData = USER_ACTIVITY.find(item => item.userId === Number(userId))
     return poidsData.sessions
 }
 
 export async function getObjectifsData(userId) {
-    const objectifsData = USER_AVERAGE_SESSIONS.find(item => item.userId == userId)
+    const objectifsData = USER_AVERAGE_SESSIONS.find(item => item.userId === Number(userId))
     return objectifsData.sessions
 }
 
 export async function getPerformanceData(userId) {
-    const performanceData = USER_PERFORMANCE.find(item => item.userId == userId)
-    return performanceData.data
-}
-
-export async function getScore(id) {
-    const userScore = USER_MAIN_DATA.find(item => item.id == id)
-    return userScore
+    const performanceData = USER_PERFORMANCE.find(item => item.userId === Number(userId))
+    return performanceData
 }
